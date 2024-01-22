968f5a6e750ae75243b3a5d75d2f4010e983808b
PabloRafael-coder <pabloofael@gmail.com>
2024-01-22 19:33:48 -0300

    new alteration

--- src/app/controllers/Usercontroller.js
+++ src/app/controllers/Usercontroller.js
@@ -1,23 +1,42 @@
-import { v4 } from 'uuid';
-import User from '../models/User';
-
+    import { v4 } from 'uuid';
+    import User from '../models/User';
+    import * as Yup from 'yup'
 
     class UserController {
+        async store(request, response) {
 
-        async store( request, response) {
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                email: Yup.string().email().required(),
+                password_hash: Yup.string().required().min(6),
+                admin: Yup.boolean()
+            })
 
-            const {name, email, password_hash, admin } = request.body
+            try {
+                await schema.validateSync(request.body, {abortEarly: false})
+            } catch (error) {
+                return response.status(400).json({error: error.errors})
+            }
+
+            const { name, email, password_hash, admin } = request.body;
+
+            const userExists = await User.findOne ({
+                where: {email},
+            })
+
+            if(userExists) {
+                return response.status(400).json({error: "Email already exists"})
+            }
 
             const user = await User.create({
-                id: v4(),
-                name,
-                email,
-                password_hash,
-                admin,
-            })
-            return response.json(user)
+            id: v4(),
+            name,
+            email,
+            password_hash,
+            admin,
+            });
+            return response.json(user);
+        }
         }
 
-    }
-
-    export default new UserController()
+    export default new UserController();
